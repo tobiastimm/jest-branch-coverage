2ccf64a9b50878af03fd95e3fd10b694f170c5db
Tobias Timm <tbs.timm@gmail.com>
2020-06-01 18:28:05 +0200

    :white_check_mark: Updating tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,5 @@
+const execa = require('execa')
+const process = require('process')
 const {
   changedFiles,
   findRelatedTests,
@@ -5,14 +7,68 @@ const {
 } = require('../lib')
 
 describe('branch-coverage', () => {
+  beforeEach(jest.restoreAllMocks)
+
   describe('changedFiles', () => {
     it('should be defined', () => {
       expect(changedFiles).toBeDefined()
     })
 
-    it('should print the changed files', async () => {
-      const result = await changedFiles('feature/example', 'master')
-      expect(result).toEqual(['example/example.js', 'example/example.ts'])
+    it('should return an array of changes', async () => {
+      const commandSpy = jest
+        .spyOn(execa, 'command')
+        .mockImplementationOnce(() => {
+          return Promise.resolve({ stdout: 'feature/a.js\nfeature/b.js' })
+        })
+
+      const mockBranchName = 'feature/example'
+      const mockOrigin = 'master'
+
+      const result = await changedFiles(mockBranchName, mockOrigin)
+        .run()
+        .promise()
+      expect(result).toEqual(['feature/a.js', 'feature/b.js'])
+      expect(commandSpy).toHaveBeenCalledWith(
+        `git diff --name-only ${mockBranchName} ${mockOrigin}`
+      )
+    })
+
+    it('should return an empty list, if nothing changed', async () => {
+      const commandSpy = jest
+        .spyOn(execa, 'command')
+        .mockImplementationOnce(() => {
+          return Promise.resolve({ stdout: '' })
+        })
+
+      const mockBranchName = 'feature/example'
+      const mockOrigin = 'master'
+
+      const result = await changedFiles(mockBranchName, mockOrigin)
+        .run()
+        .promise()
+      expect(result).toEqual([])
+      expect(commandSpy).toHaveBeenCalledWith(
+        `git diff --name-only ${mockBranchName} ${mockOrigin}`
+      )
+    })
+
+    it('should fail with an error, if something goes wrong', async () => {
+      const commandSpy = jest
+        .spyOn(execa, 'command')
+        .mockImplementationOnce(() => {
+          return Promise.reject({ message: 'Failed!' })
+        })
+      const mockBranchName = 'feature/example'
+      const mockOrigin = 'master'
+
+      try {
+        await changedFiles(mockBranchName, mockOrigin).run().promise()
+        expect(commandSpy).toHaveBeenCalledWith(
+          `git diff --name-only ${mockBranchName} ${mockOrigin}`
+        )
+      } catch (error) {
+        expect(error).toEqual({ message: 'Failed!' })
+      }
     })
   })
 
@@ -22,14 +78,26 @@ describe('branch-coverage', () => {
     })
 
     it('should list the related tests', async () => {
-      const result = await findRelatedTests('lib/index.js')
+      const jestCommandSpy = jest
+        .spyOn(execa, 'command')
+        .mockImplementationOnce(() => {
+          return Promise.resolve({
+            stdout:
+              '/Users/jest-branch-coverage/workspace/branch-test-coverage/test/index.test.js',
+          })
+        })
+
+      const mockChangedFiles = ['lib/index.js']
+      const result = await findRelatedTests(mockChangedFiles).run().promise()
       expect(result).toEqual({
-        changedFiles: ['lib/index.js'],
+        changedFiles: mockChangedFiles,
         relatedTests: [
           '/Users/jest-branch-coverage/workspace/branch-test-coverage/test/index.test.js',
-          '/Users/jest-branch-coverage/workspace/branch-test-coverage/test/bla.test.js',
         ],
       })
+      expect(jestCommandSpy).toHaveBeenCalledWith(
+        `jest --listTests --findRelatedTests ${mockChangedFiles.join(' ')}`
+      )
     })
   })
 
@@ -38,7 +106,19 @@ describe('branch-coverage', () => {
       expect(collectCoverageFromChanges).toBeDefined()
     })
 
-    it('should return the coverage', () => {
+    it('should return the coverage', async () => {
+      const jestCommandSpy = jest
+        .spyOn(execa, 'command')
+        .mockImplementationOnce(() => {
+          return Promise.resolve({
+            stdout: '',
+          })
+        })
+      const processSpy = jest
+        .spyOn(process, 'cwd')
+        .mockImplementation(
+          () => '/Users/jest-branch-coverage/workspace/branch-test-coverage/'
+        )
       const mockChanges = {
         changedFiles: ['lib/index.js'],
         relatedTests: [
@@ -46,7 +126,14 @@ describe('branch-coverage', () => {
           '/Users/jest-branch-coverage/workspace/branch-test-coverage/test/bla.test.js',
         ],
       }
-      expect(collectCoverageFromChanges(mockChanges)).toEqual()
+      const result = await collectCoverageFromChanges(mockChanges)
+        .run()
+        .promise()
+      expect(result).toEqual('')
+      expect(processSpy).toHaveBeenCalled()
+      expect(jestCommandSpy).toHaveBeenCalledWith(
+        'jest --collectCoverageFrom lib/index.js --coverage test/index.test.js test/bla.test.js'
+      )
     })
   })
 })
